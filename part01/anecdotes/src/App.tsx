dd3340c1ac92ae375eee6e7a45f6b7157b45509c
CarlosPProjects <cg9822@gmail.com>
2024-09-24 22:12:39 +0200

    "Removed unnecessary whitespace and added vote count display"

--- part01/anecdotes/src/App.tsx
+++ part01/anecdotes/src/App.tsx
@@ -4,9 +4,7 @@ import anecdotes from "./data/anecdotes";
 
 function App() {
   const [selected, setSelected] = useState(0);
-  const [points, setPoints] = useState(
-    Array(anecdotes.length).fill(0)
-  );
+  const [points, setPoints] = useState(Array(anecdotes.length).fill(0));
 
   const handleClick = () => {
     setSelected(getRandomIndexFromArray(anecdotes));
@@ -16,12 +14,13 @@ function App() {
     const newPoints = [...points];
     newPoints[selected] += 1;
     setPoints(newPoints);
-  }
+  };
 
   return (
     <div>
       {anecdotes[selected]}
       <br />
+      {points[selected] > 0 ? (<p>Has {points[selected]} votes</p>) : ""}
       <button onClick={handleClick}>Next anecdote</button>
       <button onClick={handleVote}>Vote</button>
       <button onClick={() => console.log(points)}>show</button>
