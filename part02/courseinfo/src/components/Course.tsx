75453454f440aec036526301d806f5e2f0820375
CarlosPProjects <cg9822@gmail.com>
2024-09-25 18:27:22 +0200

    Initial Commit "course info" part02

---
+++ part02/courseinfo/src/components/Course.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+interface CourseProps {
+  
+}
+
+const Course: FC<CourseProps> = ({  }) => {
+  return (
+    <div>
+     Course
+    </div>
+  )
+}
+
+export default Course;
---
+++ part02/courseinfo/src/components/Part.tsx
@@ -0,0 +1,15 @@
+import { FC } from 'react'
+
+interface PartProps {
+  
+}
+
+const Part: FC<PartProps> = ({  }) => {
+  return (
+    <div>
+     Part
+    </div>
+  )
+}
+
+export default Part;
